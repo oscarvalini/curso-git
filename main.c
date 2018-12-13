d333686282c28a59f64918806e9769f7e0f5e50e
Oscar Valini <oscar.valini@gmail.com>
2018-12-13 03:07:42 -0200

    Leitura do comando inicial

--- main.c
+++ main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void deleta(void);
+void adiciona(void);
 
 int
 main(void) {
 
-	puts("Programa legal");
+	char comando[4];
+	puts(	"add - Adiciona elemento\n"
+		"del - remove um elemento\n"
+		"Digite um comando: ");
+	fgets(comando, 4, stdin);
+
+	if (strcmp(comando, "add") == 0) {
+		adiciona();
+	}
+	else if (strcmp(comando, "del") == 0) {
+		deleta();
+	}
+	else{
+		puts("Comando inválido");
+	}
+
 	return 0;
 
+}
+
+void adiciona() {
+	puts("Funcao adiciona invocada");
+	return;
+}
+
+void deleta() {
+	puts("Funcao remove invocada");
+	return;
 }
