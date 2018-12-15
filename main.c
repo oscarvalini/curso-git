0f54416abcb22e7dea86ed54540a32429a3b8464
Oscar Valini <oscar.valini@gmail.com>
2018-12-15 02:40:32 -0200

    adiciona funcoes importantes

--- main.c
+++ main.c
@@ -3,13 +3,17 @@
 #include <string.h>
 
 struct node {
-	char *nome;
+	char nome[30];
 	struct node *prox;
 };
 typedef struct node Node;
 
+int tamanho;
+
 void deleta(void);
-void adiciona(void);
+void adiciona(Node**, int, char*);
+void adiciona_fim(Node**, char*);
+void list(Node**);
 
 int
 main(void) {
@@ -21,12 +25,12 @@ main(void) {
 	char nome[30];
 
 	do {
-		puts(	"\nadd [num] - Adiciona elemento\n"
+		/* puts(	"\nadd [num] - Adiciona elemento\n"
 				"addAt [pos] [nome]\n"
 				"del [num] - remove um elemento\n"
 				"delAt [pos] [nome]\n"
 				"quit - deixar o programa\n"
-				"Digite um comando:");
+				"Digite um comando:"); */
 
 		fgets(comando, 50, stdin);
 
@@ -34,28 +38,28 @@ main(void) {
 
 		if(strcmp(func, "addAt") == 0 )  {
 			sscanf(comando, "%s%d%s", func, &pos, nome);
-			puts("Cheguei aqui");
 		}
 		else if (strcmp(func, "delAt") == 0) {
-			sscanf(comando, "%s%d", func, pos);
+			sscanf(comando, "%s%d", func, &pos);
 		}
 		else {
 			sscanf(comando, "%s%s", func, nome);
 		}
 
-		puts(func);
-
 		if (strcmp(func, "add") == 0) {
-			adiciona();
+			adiciona_fim(&lista, nome);
 		}
 		else if (strcmp(func, "addAt") == 0) {
-			adiciona();
+			adiciona(&lista, pos, nome);
 		}
 		else if (strcmp(func, "del") == 0) {
 			deleta();
 		}
 		else if (strcmp(func, "delAt") == 0) {
 			deleta();
+		}
+		else if (strcmp(func, "lista") == 0) {
+			list(&lista);
 		}
 		else{
 			if(strcmp(func, "quit") != 0)
@@ -68,15 +72,62 @@ main(void) {
 
 }
 
-void adiciona(Node **Lista, int pos, char *nome) {
+void adiciona(Node **lista, int pos, char nome[30]) {
 	
 	Node *novo = (Node*) malloc(sizeof(Node));
+	Node *atual = *lista;
+	Node *ant = NULL;
+
 	if(novo == NULL) {
-		return 1;
+		return;
 	}
-	novo->nome = nome;
+	strcpy(novo->nome, nome);
 	novo->prox = NULL;
 
+	if(*lista == NULL || pos == 1){
+		Node *temp = *lista;
+		*lista = novo;
+		novo->prox = temp;
+		return;
+	}
+
+	int i;
+	for(i = 1; i < pos && atual != NULL; ++i ) {
+		ant = atual;
+		atual = atual->prox;
+	}
+	if(i != pos) {
+		puts("Posicao invalida!");
+		return;
+	}
+	ant->prox = novo;
+	novo->prox = atual;
+	
+	return;
+}
+
+void adiciona_fim(Node **lista, char *nome) {
+	Node *novo = (Node*) malloc(sizeof(Node));
+	Node *atual = *lista;
+	Node *ant = NULL;
+
+	if(novo == NULL) {
+		return;
+	}
+	strcpy(novo->nome, nome);
+	novo->prox = NULL;
+
+	if(*lista == NULL){
+		*lista = novo;
+		return;
+	}
+
+	while (atual != NULL) {
+		ant = atual;
+		atual = atual->prox;
+	}
+		ant->prox = novo;
+		novo->prox = atual;
 	
 	return;
 }
@@ -84,4 +135,14 @@ void adiciona(Node **Lista, int pos, char *nome) {
 void deleta() {
 	puts("Funcao remove invocada");
 	return;
+}
+
+void list(Node **lista) {
+	Node *atual = *lista;
+
+	while (atual != NULL) {
+		printf("%s -> ", atual->nome);
+		atual = atual->prox;
+	}
+	puts("FIM");
 }
