97d6ce65692e2abe829efe21cfff8a75a5f21ea0
Oscar Valini <oscar.valini@gmail.com>
2018-12-14 22:29:50 -0200

    lista

--- main.c
+++ main.c
@@ -2,34 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct node {
+	char *nome;
+	struct node *prox;
+};
+typedef struct node Node;
+
 void deleta(void);
 void adiciona(void);
 
 int
 main(void) {
 
-	char comando[4];
-	puts(	"add - Adiciona elemento\n"
-		"del - remove um elemento\n"
-		"Digite um comando: ");
-	fgets(comando, 4, stdin);
+	Node *lista = NULL;
+	char comando[50];
+	char func[6];
+	int pos;
+	char nome[30];
 
-	if (strcmp(comando, "add") == 0) {
-		adiciona();
-	}
-	else if (strcmp(comando, "del") == 0) {
-		deleta();
-	}
-	else{
-		puts("Comando inválido");
-	}
+	do {
+		puts(	"\nadd [num] - Adiciona elemento\n"
+				"addAt [pos] [nome]\n"
+				"del [num] - remove um elemento\n"
+				"delAt [pos] [nome]\n"
+				"quit - deixar o programa\n"
+				"Digite um comando:");
+
+		fgets(comando, 50, stdin);
+
+		sscanf(comando,"%s", func);
+
+		if(strcmp(func, "addAt") == 0 )  {
+			sscanf(comando, "%s%d%s", func, &pos, nome);
+			puts("Cheguei aqui");
+		}
+		else if (strcmp(func, "delAt") == 0) {
+			sscanf(comando, "%s%d", func, pos);
+		}
+		else {
+			sscanf(comando, "%s%s", func, nome);
+		}
+
+		puts(func);
+
+		if (strcmp(func, "add") == 0) {
+			adiciona();
+		}
+		else if (strcmp(func, "addAt") == 0) {
+			adiciona();
+		}
+		else if (strcmp(func, "del") == 0) {
+			deleta();
+		}
+		else if (strcmp(func, "delAt") == 0) {
+			deleta();
+		}
+		else{
+			if(strcmp(func, "quit") != 0)
+				puts("Comando inválido");
+		}
+
+	}while(strcmp(func, "quit") != 0);
 
 	return 0;
 
 }
 
-void adiciona() {
-	puts("Funcao adiciona invocada");
+void adiciona(Node **Lista, int pos, char *nome) {
+	
+	Node *novo = (Node*) malloc(sizeof(Node));
+	if(novo == NULL) {
+		return 1;
+	}
+	novo->nome = nome;
+	novo->prox = NULL;
+
+	
 	return;
 }
 
